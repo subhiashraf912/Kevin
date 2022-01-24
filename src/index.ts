a0be73a193932f9711bdf4b2bbd0decaf94d67bd
subhiashraf912 <subhi.ashraf912@gmail.com>
2022-01-24 17:32:29 +0200

    bruh

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
-import { registerCommands, registerEvents } from "./utils/registry";
+import {
+  registerCommands,
+  registerEvents,
+  // registerWebSocketEvents,
+} from "./utils/registry";
 import config from "../slappey.json";
 import DiscordClient from "./classes/Client/Client";
 import dotenv from "dotenv";
@@ -13,6 +17,21 @@ const client = new DiscordClient({
   client.prefix = config.prefix || client.prefix;
   await registerCommands(client, "../commands");
   await registerEvents(client, "../events");
+  // await registerWebSocketEvents(client, "../WebSocketEvents");
   await client.database.init();
   await client.login(process.env.BOT_TOKEN);
 })();
+
+client.on("voiceStateUpdate", (oldState, newState) => {
+  try {
+    if (oldState.guild.id !== "783991881028993045") return;
+    if (!oldState.channel && newState.channel) {
+      const role = newState.guild.roles.cache.get("933290405116665876");
+      if (role) newState.member?.roles.add(role);
+    }
+    if (oldState.channel && !newState.channel) {
+      const role = newState.guild.roles.cache.get("933290405116665876");
+      if (role) newState.member?.roles.remove(role);
+    }
+  } catch {}
+});
