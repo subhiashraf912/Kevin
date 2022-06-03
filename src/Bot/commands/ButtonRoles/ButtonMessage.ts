2667d871b6922f34db33008dff46a2454d3ddd1d
subhi.ashraf912 <subhi.ashraf912@gmail.com>
2022-06-03 13:41:38 +0300

    asfa

--- src/Bot/commands/ButtonRoles/ButtonMessage.ts
+++ src/Bot/commands/ButtonRoles/ButtonMessage.ts
@@ -1,7 +1,19 @@
-import { Message, TextChannel } from "discord.js";
+import {
+  CollectorFilter,
+  Message,
+  MessageAttachment,
+  MessageCollector,
+  TextChannel,
+} from "discord.js";
 import BaseCommand from "../../classes/Base/BaseCommand";
 import DiscordClient from "../../classes/Client/Client";
 import PermissionsGuard from "../../classes/Guard/PermissionsGuard";
+
+type questionType = {
+  question: string;
+  answer: Message | null;
+};
+
 export default class Command extends BaseCommand {
   constructor() {
     super({
@@ -15,28 +27,74 @@ export default class Command extends BaseCommand {
   }
 
   async run(client: DiscordClient, message: Message, args: Array<string>) {
-    const argmnts = args;
-    const channel: TextChannel =
-      (message.mentions.channels.first() as TextChannel) ||
-      (message.channel as TextChannel);
-    if (message.mentions.channels.first()) {
-      argmnts.splice(
-        args.indexOf(message.mentions.channels.first()?.toString()!),
-        1
-      );
+    try {
+      const questions: questionType[] = [
+        {
+          question:
+            "Enter the channel you want to send the text message in, use id/name/#channel, type 'this' to use this channel.",
+          answer: null,
+        },
+        {
+          question: "Enter the text of the message you want.",
+          answer: null,
+        },
+        {
+          question:
+            "If you want to send an image with the message, send it as image or send it's link. if not type 'none'",
+          answer: null,
+        },
+      ];
+
+      const filter: CollectorFilter<[Message<boolean>]> = (filteredMessae) =>
+        filteredMessae.author.id === message.author.id;
+      const collector = new MessageCollector(message.channel, {
+        filter,
+        max: questions.length,
+      });
+      let i = 0;
+      await message.channel.send({ content: questions[i].question });
+      collector.on("collect", async (collectedMessage) => {
+        questions[i].answer = collectedMessage;
+        i++;
+        if (i < questions.length)
+          await message.channel.send(questions[i].question);
+      });
+
+      const invokeCollectorCallBack = async () => {
+        const channel =
+          (questions[0].answer?.mentions.channels.first() as TextChannel) ||
+          questions[0].answer?.guild?.channels.cache.find(
+            (c) =>
+              c.name.toLowerCase() ===
+                questions[0].answer?.content.toLowerCase() ||
+              questions[0].answer?.content.toLowerCase() === c.id.toLowerCase()
+          ) ||
+          message.channel;
+
+        const text = questions[1].answer?.content!;
+        const attachment = new MessageAttachment(
+          message.attachments.first()?.url || message.content
+        );
+        const sentMessage = await channel.send({
+          content: text,
+          attachments: [attachment],
+        });
+        const channelId = channel.id;
+        const guildId = message.guildId!;
+        const messageId = sentMessage.id;
+        await client.database.models.buttonRoles.create({
+          channelId,
+          guildId,
+          messageId,
+        });
+
+        message.reply({ content: "Message Sent." });
+      };
+      collector.on("end", async (col) => {
+        invokeCollectorCallBack();
+      });
+    } catch (err: any) {
+      message.reply(`Error: ${err.message}`);
     }
-
-    const text = argmnts.join(" ");
-    const sentMessage = await channel.send({ content: text });
-    const channelId = channel.id;
-    const guildId = message.guildId!;
-    const messageId = sentMessage.id;
-    await client.database.models.buttonRoles.create({
-      channelId,
-      guildId,
-      messageId,
-    });
-
-    message.reply({ content: "Message Sent." });
   }
 }
