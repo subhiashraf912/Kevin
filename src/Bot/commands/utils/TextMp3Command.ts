7751f60f274155774b418c1ab6ed398938b2a558
subhi.ashraf912 <subhi.ashraf912@gmail.com>
2022-06-19 13:39:18 +0300

    bra

--- src/Bot/commands/utils/TextMp3Command.ts
+++ src/Bot/commands/utils/TextMp3Command.ts
@@ -18,8 +18,27 @@ export default class HelpCommand extends BaseCommand {
 
   async run(client: DiscordClient, message: Message, args: Array<string>) {
     if (!args[0]) return message.reply("You need to enter some text...");
+
+    const res = await fetch("https://ttsmp3.com/makemp3_new.php", {
+      headers: {
+        accept: "*/*",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/x-www-form-urlencoded",
+        "sec-ch-ua":
+          '" Not A;Brand";v="99", "Chromium";v="101", "Opera";v="87"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        Referer: "https://ttsmp3.com/",
+        "Referrer-Policy": "strict-origin-when-cross-origin",
+      },
+      body: `msg=${args.join(" ")}&lang=Joanna&source=ttsmp3`,
+      method: "POST",
+    });
+
     const data = { msg: args.join(" "), lang: "Joanna", source: "ttsmp3" };
-    const res = await axios.post("https://ttsmp3.com/makemp3_new.php", data);
-    console.log(res.data);
+    console.log(res.text);
   }
 }
